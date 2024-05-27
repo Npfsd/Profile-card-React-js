a5e27d4f3fb04ce0b3bd05b56ad065a1202dc361
Npfsd <perumalparasu262004@gmail.com>
2024-05-27 07:16:06 +0530

    updatebase path to fix incorrect assets link

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"Profile-card-React-js"
+  base:'/profile-card-react-js/',
+
 })
